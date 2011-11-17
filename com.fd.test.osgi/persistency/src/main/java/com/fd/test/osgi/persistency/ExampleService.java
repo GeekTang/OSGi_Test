9207a5ebc20a5b78641aad6add855ccf42b444ea
Jason Tang <ares.tang@gmail.com>
2011-11-17 23:00:12 +0800

    create osgi project and bundle with pax plugin

---
+++ com.fd.test.osgi/persistency/src/main/java/com/fd/test/osgi/persistency/ExampleService.java
@@ -0,0 +1,12 @@
+package com.fd.test.osgi.persistency;
+
+/**
+ * Public API representing an example OSGi service
+ */
+public interface ExampleService
+{
+    // public methods go here...
+
+    String scramble( String text );
+}
+
